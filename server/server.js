7128f3802a15eeda6f68e53363fe7a55a1bdfec6
ADOwen <92840021+ADOwen@users.noreply.github.com>
2024-11-20 15:16:20 -0600

    a temporary clien page is now being served

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
+import path from 'path'
+import { fileURLToPath } from 'url';
 import express from 'express'
 
-// DEPENDENCIES
+// APP
 const app = express();
+
+// MIDDLEWARE
+
+// server public folder
+app.use(express.static("public"));
+
+// get the current directory of the file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = 3001;
 
 app.get('/', (req, res) => {
-  res.send("Hello World");
+
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
 });
 
 app.listen(PORT, () => {
